225635a943944b533347369c4b981c14e71c25e3
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-03-12 12:55:39 +0100

    add list users and movies pages

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 from datamanager.data_manager import SQLiteDataManager
-
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, 'data', 'movies.sqlite')
 
@@ -15,6 +13,19 @@ data_manager = SQLiteDataManager(db_path, app)
 @app.route('/')
 def home():
     return "Welcome to MovieWeb App!"
+
+
+@app.route('/users')
+def list_users():
+    users = data_manager.get_all_users()
+    return render_template('users.html', users=users)
+
+
+@app.route('/users/<int:user_id>')
+def user_movies(user_id):
+    movies = data_manager.get_user_movies(user_id)
+    user = data_manager.get_user(user_id)
+    return render_template('user_movies.html', movies=movies, user=user)
 
 if __name__=="__main__":
     app.run(debug=True)
