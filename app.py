634695a2cb4cfff1bf63d657af96bbeb1833a97c
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-03-12 13:56:00 +0100

    add user feature

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 from datamanager.data_manager import SQLiteDataManager
+from datamanager.data_models import User
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, 'data', 'movies.sqlite')
 
@@ -23,9 +25,22 @@ def list_users():
 
 @app.route('/users/<int:user_id>')
 def user_movies(user_id):
+    message = request.args.get("message")
     movies = data_manager.get_user_movies(user_id)
     user = data_manager.get_user(user_id)
-    return render_template('user_movies.html', movies=movies, user=user)
+    return render_template('user_movies.html', movies=movies, user=user, message=message)
+
+
+@app.route('/add_user', methods=["GET","POST"])
+def add_user():
+    if request.method == "GET":
+        return render_template('add_user.html')
+    else:
+        form = dict(request.form)
+        user = User(**form)
+        data_manager.add_user(user)
+        message=f"Welcome {user.name} this is your movies page. Start adding your favorite movies!"
+        return redirect(url_for("user_movies", user_id=user.id, message=message))
 
 if __name__=="__main__":
     app.run(debug=True)
